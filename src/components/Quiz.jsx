a62110cadaf53ed6865918b6e2ffc6f290d27588
Asif <asifjirayat@gmail.com>
2025-08-29 16:15:01 +0530

    Display quiz completed upon completion.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,24 +1,40 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
-import { act } from "react";
+import quizComplete from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
+  // Function handle answer selection
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   };
 
+  // If quiz array exhausted, quiz completed to be rendered
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizComplete} alt="Trophy" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  // Shuffle answers everytime
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          {shuffledAnswers.map((answer, index) => (
             <li key={index} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
